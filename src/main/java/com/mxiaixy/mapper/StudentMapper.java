99e78aa253a8fc7620b214b0de35740f857f1840
xuxiaoyang21 <1334909533@qq.com>
2017-01-05 16:36:25 +0800

    添加注解缓存设置

--- src/main/java/com/mxiaixy/mapper/StudentMapper.java
+++ src/main/java/com/mxiaixy/mapper/StudentMapper.java
@@ -1,9 +1,7 @@
 package com.mxiaixy.mapper;
 
 import com.mxiaixy.pojo.Student;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -11,9 +9,12 @@ import java.util.List;
  * 测试注解配置接口
  * Created by Mxia on 2017/1/5.
  */
+@CacheNamespace
 public interface StudentMapper {
 
+    //添加注解缓存设置
     @Select("select * from t_student")
+    @Options(useCache = false,fetchSize = 1024,flushCache = Options.FlushCachePolicy.FALSE)
     List<Student> findAll();
 
     @Insert("insert into t_student(name ,address) values(#{name},#{address})")
